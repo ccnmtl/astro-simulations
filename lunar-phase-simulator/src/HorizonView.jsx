8d341101baa223bd723e34f3999bd3d553a23cf4
Nik Nyby <nnyby@columbia.edu>
2018-07-18 11:45:10 -0400

    Add circular plane and sprite to horizon

--- lunar-phase-simulator/src/HorizonView.jsx
+++ lunar-phase-simulator/src/HorizonView.jsx
@@ -9,59 +9,86 @@ export default class HorizonView extends React.Component {
         super(props)
 
         this.id = 'HorizonView';
-        this.start = this.start.bind(this)
-        this.stop = this.stop.bind(this)
-        this.animate = this.animate.bind(this)
+        this.start = this.start.bind(this);
+        this.stop = this.stop.bind(this);
+        this.animate = this.animate.bind(this);
     }
 
     componentDidMount() {
-        const width = this.mount.clientWidth
-        const height = this.mount.clientHeight
+        const width = this.mount.clientWidth;
+        const height = this.mount.clientHeight;
 
-        const scene = new THREE.Scene()
+        const scene = new THREE.Scene();
         const camera = new THREE.PerspectiveCamera(
             75,
             width / height,
             0.1,
             1000
-        )
-        const controls = new THREE.OrbitControls(
-            camera, document.getElementById(this.id));
+        );
+        const controls = new THREE.OrbitControls(camera, this.mount);
         // Configure the controls - we only need some basic
         // drag-rotation behavior.
         controls.enableKeys = false;
         controls.enablePan = false;
         controls.enableZoom = false;
 
-        const renderer = new THREE.WebGLRenderer({ antialias: true })
-        const geometry = new THREE.BoxGeometry(1, 1, 1)
-        const material = new THREE.MeshBasicMaterial({ color: '#433F81' })
-        const cube = new THREE.Mesh(geometry, material)
+        // Only let the user see the top of the scene - no need to
+        // flip it completely over.
+        controls.minPolarAngle = Math.PI / 4;
+        controls.maxPolarAngle = Math.PI - 1.2;
 
-        camera.position.z = 4
+        // Don't rotate on the side-to-side axis. For this mouse
+        // movement, the plane must be rotated on its own axis.
+        controls.minAzimuthAngle = 0;
+        controls.maxAzimuthAngle = 0;
+
+        const renderer = new THREE.WebGLRenderer({ antialias: true });
+
+        camera.position.z = 8;
         controls.update();
-        scene.add(cube)
-        renderer.setClearColor('#000000')
-        renderer.setSize(width, height)
+        renderer.setClearColor(0xffffff);
+        renderer.setSize(width, height);
 
-        this.scene = scene
-        this.camera = camera
-        this.renderer = renderer
-        this.material = material
-        this.cube = cube
+        // Draw horizon
+        const texture = new THREE.TextureLoader().load('img/plane.svg');
+        const material = new THREE.MeshBasicMaterial({
+            transparent: true,
+            map: texture,
+            side: THREE.DoubleSide
+        });
+        const geometry = new THREE.CircleGeometry(5, 64);
+        const plane = new THREE.Mesh(geometry, material);
+        plane.rotation.x = -1;
+        scene.add(plane);
 
-        this.mount.appendChild(this.renderer.domElement)
-        this.start()
+        // Draw stick figure
+        const spriteMap = new THREE.TextureLoader().load('img/stickfigure.svg');
+        const spriteMaterial = new THREE.SpriteMaterial({
+            map: spriteMap,
+            color: 0xffffff
+        });
+        const sprite = new THREE.Sprite(spriteMaterial);
+        sprite.scale.set(1, 2, 1);
+        sprite.position.y = 1;
+        scene.add(sprite);
+
+        this.scene = scene;
+        this.camera = camera;
+        this.renderer = renderer;
+        this.material = material;
+
+        this.mount.appendChild(this.renderer.domElement);
+        this.start();
     }
 
     componentWillUnmount() {
-        this.stop()
-        this.mount.removeChild(this.renderer.domElement)
+        this.stop();
+        this.mount.removeChild(this.renderer.domElement);
     }
 
     start() {
         if (!this.frameId) {
-            this.frameId = requestAnimationFrame(this.animate)
+            this.frameId = requestAnimationFrame(this.animate);
         }
     }
 
@@ -70,15 +97,12 @@ export default class HorizonView extends React.Component {
     }
 
     animate() {
-        this.cube.rotation.x += 0.01
-        this.cube.rotation.y += 0.01
-
-        this.renderScene()
-        this.frameId = window.requestAnimationFrame(this.animate)
+        this.renderScene();
+        this.frameId = window.requestAnimationFrame(this.animate);
     }
 
     renderScene() {
-        this.renderer.render(this.scene, this.camera)
+        this.renderer.render(this.scene, this.camera);
     }
 
     render() {
