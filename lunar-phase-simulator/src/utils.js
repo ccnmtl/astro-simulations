8493db58c3db378480503e55b31a9d64b79b06b5
Nik Nyby <nnyby@columbia.edu>
2018-08-09 14:59:34 -0400

    Improve moon phase accuracy

--- lunar-phase-simulator/src/utils.js
+++ lunar-phase-simulator/src/utils.js
@@ -44,7 +44,95 @@ const roundToOnePlace = function(n) {
     return Math.round(n * 10) / 10;
 }
 
+/*
+ * Given the exact moon phase, return the broad bucket that this
+ * phase is called in this interactive. This is used to keep the
+ * phase dropdown up to date with the current scenario.
+ *
+ * The waning and waxing slots are larger than the others.
+ *
+ * Here's a visualization of moonPhase is represented. The circle
+ * is the orbit of the moon around the sun - seen in MainView.
+ * This is in degrees, but the moonPhase state value is in radians.
+ * Units are interchangeable.
+ *
+ * The sun is to the left of the diagram, so at 0 degrees, the moon is
+ * fully illuminated by the sun to the earth's perspective, making a
+ * full moon.
+ *
+ *
+ *                  90
+ *
+ *             . -- ~~~ -- .
+ *         .-~               ~-.
+ *        /                     \
+ *       /         .____.        \
+ *  180 |         /      \        |
+ * -180 |        (  earth )       | 0
+ *      |         \      /        |
+ *       \          `---`        /
+ *        \                     /
+ *         `-.               .-'
+ *             ~- . ___ . -~
+ *
+ *                  -90
+ *
+ *
+ * New Moon, Full Moon, and the first and third quarters are more
+ * specific, narrow events in reality, so these are represented by
+ * only 5 degrees around this orbit. The waxing and waning phases
+ * take up the rest of the orbit.
+ */
+const getPhaseSlot = function(moonPhase) {
+    const phase = radToDeg(moonPhase);
+
+    // New Moon
+    if (Math.abs(phase - 180) < 5 ||
+        Math.abs(phase + 180) < 5
+       ) {
+        return 180;
+    }
+
+    // First Quarter
+    if (Math.abs(phase + 90) < 5) {
+        return -90;
+    }
+
+    // Full Moon
+    if (Math.abs(phase) < 5) {
+        return 0;
+    }
+
+    // Third Quarter
+    if (Math.abs(phase - 90) < 5) {
+        return 90;
+    }
+
+    // Waxing Crescent
+    if (Math.abs(phase + 135) < 45) {
+        return -135;
+    }
+
+    // Waxing Gibbous
+    if (Math.abs(phase + 45) < 45) {
+        return -45;
+    }
+
+    // Waning Gibbous
+    if (Math.abs(phase - 45) < 45) {
+        return 45;
+    }
+
+    // Waning Crescent
+    if (Math.abs(phase - 135) < 45) {
+        return 135;
+    }
+
+    // error
+    return null;
+}
+
 export {
     forceNumber, loadSprite, degToRad, radToDeg, getPercentIlluminated,
-    roundToOnePlace
+    roundToOnePlace, getPhaseSlot
 };
--- lunar-phase-simulator/src/utils.test.js
+++ lunar-phase-simulator/src/utils.test.js
@@ -1,9 +1,10 @@
 /* eslint-env jest */
 
-import {degToRad, getPercentIlluminated} from './utils';
+import {degToRad, getPercentIlluminated, getPhaseSlot} from './utils';
 
 test('degToRad computes radians correctly', () => {
     expect(degToRad(180)).toBe(Math.PI);
+    expect(degToRad(90)).toBe(Math.PI / 2);
 });
 
 test('getPercentIlluminated acts correctly', () => {
@@ -22,4 +23,30 @@ test('getPercentIlluminated acts correctly', () => {
     expect(
         Math.round(getPercentIlluminated(Math.PI + (Math.PI / 2)))
     ).toBe(50);
+});
+
+test('getPhaseSlot acts correctly', () => {
+    // Full moon
+    expect(getPhaseSlot(degToRad(0))).toBe(0);
+    expect(getPhaseSlot(degToRad(1))).toBe(0);
+    expect(getPhaseSlot(degToRad(-2))).toBe(0);
+
+    // New moon
+    expect(getPhaseSlot(Math.PI)).toBe(180);
+    expect(getPhaseSlot(degToRad(181))).toBe(180);
+    expect(getPhaseSlot(degToRad(-183))).toBe(180);
+
+    // New moon and full moon's slot is smaller than the others.
+    expect(getPhaseSlot(Math.PI - (Math.PI / 8))).toBe(135);
+    expect(getPhaseSlot(degToRad(20))).toBe(45);
+    expect(getPhaseSlot(degToRad(60))).toBe(45);
+
+    // Third Quarter
+    expect(getPhaseSlot(Math.PI / 2)).toBe(90);
+
+    // First Quarter
+    expect(getPhaseSlot(degToRad(-92))).toBe(-90);
+    expect(getPhaseSlot(degToRad(-88))).toBe(-90);
+
+    expect(getPhaseSlot(degToRad(-80))).toBe(-45);
 });
