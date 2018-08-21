b927c779bca1419598ff12467ac5450eab251f6e
Nik Nyby <nnyby@columbia.edu>
2018-08-21 13:56:03 -0400

    Exoplanet Transit Simulator: make basic dom structure

---
+++ exoplanet-transit-simulator/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/main.jsx',
+    mode: process.env.WEBPACK_SERVE ? 'development' : 'production',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                include: path.resolve(__dirname, 'src'),
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    }
+};
