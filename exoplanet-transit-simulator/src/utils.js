5f94478ed0d9fe1e20b977ebe82a7b121114bca3
Nikolas Nyby <nikolas@gnu.org>
2018-12-07 22:48:06 -0500

    Transit sim star color: add linear interpolation

--- exoplanet-transit-simulator/src/utils.js
+++ exoplanet-transit-simulator/src/utils.js
@@ -111,6 +111,38 @@ const getSystemPeriod = function(planetSemimajorAxis, planetMass, starMass) {
     );
 };
 
+/**
+ * A linear interpolator for hex colors.
+ *
+ * Based on:
+ * https://gist.github.com/rosszurowski/67f04465c424a9bc0dae
+ *
+ * @param {Number} a  (hex color start val)
+ * @param {Number} b  (hex color end val)
+ * @param {Number} amount  (the amount to fade from a to b)
+ *
+ * @example
+ * // returns 0x7f7f7f
+ * lerpColor(0x000000, 0xffffff, 0.5)
+ *
+ * @returns {Number}
+ */
+const lerpColor = function(a, b, amount) {
+    const ar = a >> 16,
+          ag = a >> 8 & 0xff,
+          ab = a & 0xff,
+
+          br = b >> 16,
+          bg = b >> 8 & 0xff,
+          bb = b & 0xff,
+
+          rr = ar + amount * (br - ar),
+          rg = ag + amount * (bg - ag),
+          rb = ab + amount * (bb - ab);
+
+    return (rr << 16) + (rg << 8) + (rb | 0);
+};
+
 export {
     forceNumber,
     roundToOnePlace,
@@ -120,5 +152,6 @@ export {
     rJupToKm, rSunToKm,
     getDist, getPlanetY,
     getEclipseDepth,
-    getSystemPeriod
+    getSystemPeriod,
+    lerpColor
 };
