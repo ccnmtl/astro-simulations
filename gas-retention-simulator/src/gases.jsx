ce83eadf335d0f4e52f0f93b2039119a02c37218
Nikolas Nyby <nikolas@gnu.org>
2019-05-13 15:30:24 -0400

    Gas retention sim: Allow adding gases - HEL-109

---
+++ gas-retention-simulator/src/gases.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+
+const gases = [
+    {
+        id: 1,
+        particleSize: 6,
+        color: 0x6666a0,
+        name: 'Xenon',
+        symbol: 'Xe',
+        mass: 131.293
+    },
+    {
+        id: 2,
+        particleSize: 4.5,
+        color: 0xb0b000,
+        name: 'Carbon dioxide',
+        symbol: <span>CO<sub>2</sub></span>,
+        mass: 44.009500
+    },
+    {
+        id: 3,
+        particleSize: 4,
+        color: 0x00d0f0,
+        name: 'Oxygen',
+        symbol: <span>O<sub>2</sub></span>,
+        mass: 31.9988
+    },
+    {
+        id: 4,
+        particleSize: 3.5,
+        color: 0xB87B41,
+        name: 'Nitrogen',
+        symbol: <span>N<sub>2</sub></span>,
+        mass: 28.0134
+    },
+    {
+        id: 5,
+        particleSize: 3,
+        color: 0x0050ff,
+        name: 'Water',
+        symbol: <span>H<sub>2</sub>O</span>,
+        mass: 18.015280
+    },
+    {
+        id: 6,
+        particleSize: 3,
+        color: 0xa050ff,
+        name: 'Ammonia',
+        symbol: <span>NH<sub>3</sub></span>,
+        mass: 17.03052
+    },
+    {
+        id: 7,
+        particleSize: 3,
+        color: 0xff6600,
+        name: 'Methane',
+        symbol: <span>CH<sub>4</sub></span>,
+        mass: 16.042460
+    },
+    {
+        id: 8,
+        particleSize: 2.5,
+        color: 0x00aa00,
+        name: 'Helium',
+        symbol: 'He',
+        mass: 4.002602
+    },
+    {
+        id: 9,
+        particleSize: 2.5,
+        color: 0xff0000,
+        name: 'Hydrogen',
+        symbol: <span>H<sub>2</sub></span>,
+        mass: 2.01588
+    }
+];
+
+export {gases};
