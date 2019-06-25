39def93e3e47edf4958c81385b6d77c380c20b05
Nikolas Nyby <nikolas@gnu.org>
2019-06-25 14:45:18 -0400

    Enable maxwell plot line code
    It's still not displaying correctly, but now it's hooked up and on the
    right path.

--- gas-retention-simulator/src/MaxwellPlot.jsx
+++ gas-retention-simulator/src/MaxwellPlot.jsx
@@ -19,6 +19,7 @@ export default class MaxwellPlot extends React.Component {
         this._xscale = this._yscale = 100;
         this._xMin = 0;
         this._xMax = 2000;
+        this.fraction = 1;
 
         this.__yScale = -50000;
 
@@ -89,139 +90,6 @@ export default class MaxwellPlot extends React.Component {
             }
             x += xStep;
         }
-    }
-
-    update() {
-        this.curveMC.clear();
-        this.fillMC.clear();
-
-        if (this.getIsInvalid()) {
-            //		trace(" "+this._name+": -");
-            return;
-        }
-
-        var paramsObj = {};
-        paramsObj.a = this._a;
-        paramsObj.xMin = this._parent._xMin;
-        paramsObj.xMax = this._parent._xMax;
-        paramsObj.xScale = this._parent.__xScale;
-        paramsObj.yScale = this.fraction*this._parent.__yScale;
-
-        paramsObj.mc = this.curveMC;
-        this.curveMC.lineStyle(this.curveThickness, this.curveColor, this.curveAlpha);
-        this.drawMaxwell(paramsObj);
-
-        if (this.showFill) {
-            paramsObj.mc = this.fillMC;
-            this.fillMC.beginFill(this.fillColor, this.fillAlpha);
-            var startPt = this.drawMaxwell(paramsObj);
-            this.fillMC.lineStyle();
-            this.fillMC.lineTo(this._parent._plotWidth, 0);
-            this.fillMC.lineTo(0, 0);
-            this.fillMC.lineTo(startPt.x, startPt.y);
-            this.fillMC.endFill();
-        }
-
-        //	trace(" "+this._name+": "+(getTimer()-startTimer));
-    }
-
-    drawMaxwell(paramsObj) {
-        // This function draws the Maxwell-Boltzmann probability distribution using a piecewise bezier
-        // approximation. The function returns an object with x and y properties. These define the
-        // screen coordinates of the starting point of the curve (on the left side). This can be useful
-        // for closing a fill. The argument paramsObj is expected to have the following properties:
-        //   mc - the movieclip to draw the curve in; it is assumed that the movieclip is cleared and
-        //     ready for drawing (ie. lineStyle has already been called)
-        //   a - this defines the shape of the curve; in the physical context the value is sqrt(kT/m)
-        //   xMin, xMax - the minimum and maximum values of x in the plot window (in units, e.g. m/s)
-        //   xScale, yScale - the scales for the x and y axes, in px/unit (yScale should be negative)
-        //   numSegments - (optional) the number of segments (or more accurately, the minimum number of
-        //     segments) to use to draw the curve; the default is 8
-        var exp = Math.exp;
-        var mc = paramsObj.mc;
-        var a = paramsObj.a;
-        var xMin = paramsObj.xMin;
-        var xMax = paramsObj.xMax;
-        var xScale = paramsObj.xScale;
-        var yScale = paramsObj.yScale;
-        var K0 = Math.sqrt(2/Math.PI)/(a*a*a);
-        var K1 = 2*a*a;
-        var K2 = 2*(yScale/xScale);
-
-        let nTotal;
-
-        if (paramsObj.numSegments!=undefined) {
-            nTotal = paramsObj.numSegments;
-        } else {
-            nTotal = 8;
-        }
-
-        let startPt;
-
-        // after x = 5*a the curve is essentially zero for any reasonable y scaling
-        var lim = 5*a;
-        if (lim<xMin) {
-            startPt = {x: 0, y: 0};
-            mc.moveTo(0, 0);
-            mc.lineTo(xScale*(xMax-xMin), 0);
-        }
-        else {
-            // maL is a list of mandatory anchor points, excluding the starting point; this list
-            // includes the mode value (so the peak is always accurately shown) as well as the
-            // inflection points (the method used to calculate control points requires this)
-            var maL = [];
-            var inf1 = a*Math.sqrt((5-Math.sqrt(17))/2);
-            if (inf1>xMin && inf1<xMax) maL.push(inf1);
-            var xMode = a*Math.SQRT2;
-            if (xMode>xMin && xMode<xMax) maL.push(xMode);
-            var inf2 = a*Math.sqrt((5+Math.sqrt(17))/2);
-            if (inf2>xMin && inf2<xMax) maL.push(inf2);
-
-            let range;
-            let limitPassed;
-            let J0;
-            let m;
-            let ax, ay;
-
-            if (lim<xMax) {
-                maL.push(lim);
-                range = lim-xMin;
-                limitPassed = true;
-            }
-            else {
-                maL.push(xMax);
-                range = xMax-xMin;
-                limitPassed = false;
-            }
-            var x = xMin;
-            J0 = K0*x*exp(-x*x/K1);
-            m = J0*K2*(1-(x*x/K1));
-            ax = 0;
-            ay = yScale*x*J0;
-            startPt = {x: ax, y: ay};
-            mc.moveTo(ax, ay);
-            for (var i=0; i<maL.length; i++) {
-                // n is the number of curves to use for this interval
-                // xStep is the x-distance (in value units) between anchor points over this segment
-                var n = Math.ceil(nTotal*(maL[i]-x)/range);
-                var xStep = (maL[i]-x)/n;
-                for (var j=0; j<n; j++) {
-                    var lax = ax;
-                    var lay = ay;
-                    var lm = m;
-                    x += xStep;
-                    J0 = K0*x*exp(-x*x/K1);
-                    m = J0*K2*(1-(x*x/K1));
-                    ax = xScale*(x-xMin);
-                    ay = yScale*x*J0;
-                    var cx = (lay - ay - lm*lax + m*ax)/(m - lm);
-                    var cy = m*(cx - ax) + ay;
-                    mc.curveTo(cx, cy, ax, ay);
-                }
-            }
-            if (limitPassed) mc.lineTo(xScale*(xMax-xMin), 0);
-        }
-        return startPt;
     }
 
     onDragStart(e) {
