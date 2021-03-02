397dac3b7e9872b421d2e0f9faee7b701ab5ceaa
Nikolas Nyby <nikolas@gnu.org>
2021-03-02 15:37:01 -0500

    EBS: Fix rounding problem with separation field - HEL-206

--- eclipsing-binary-simulator/src/main.jsx
+++ eclipsing-binary-simulator/src/main.jsx
@@ -976,7 +976,7 @@ class EclipsingBinarySimulator extends React.Component {
         if (target.type === 'radio') {
             value = target.id === (target.name + 'Radio');
         } else if (target.type === 'range' || target.type === 'number') {
-            value = forceNumber(value);
+            value = roundToTwoPlaces(forceNumber(value));
 
             if (name === 'phase') {
                 value = Math.max(value, target.min);
