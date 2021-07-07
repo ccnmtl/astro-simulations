089d10dbe290c93ac097be9d63190a7ae7cd9440
Nick Buonincontri <nbuonin@gmail.com>
2021-07-07 10:45:00 -0400

    Bootstrap the CSHZ Sim

---
+++ circumstellar-habitable-zone-simulator/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/main.jsx',
+    mode: process.env.WEBPACK_SERVE ? 'development' : 'production',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist/',
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx'],
+        alias: {
+            'three/CopyShader': path.join(__dirname, 'node_modules/three/examples/js/shaders/CopyShader.js')
+        }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            'THREE': 'three'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                include: path.resolve(__dirname, 'src'),
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            }
+        ]
+    }
+};
