8cd26fd5a0896c99cadee62b0c9744cf2315ba8f
Nikolas Nyby <nnyby@columbia.edu>
2026-02-25 13:36:00 -0500

    Remove eslint-plugin-security
    This plugin is unmaintained, not really necessary, and blocking an
    eslint update.

--- hydrogen-atom-simulator/eslint.config.js
+++ hydrogen-atom-simulator/eslint.config.js
@@ -1,12 +1,10 @@
 const js = require("@eslint/js");
 const globals = require("globals");
 const reactPlugin = require("eslint-plugin-react");
-const securityPlugin = require("eslint-plugin-security");
 const babelParser = require("@babel/eslint-parser");
 
 module.exports = [
   js.configs.recommended,
-  securityPlugin.configs.recommended,
   reactPlugin.configs.flat.recommended,
   {
     files: ["**/*.js", "**/*.jsx"],
@@ -50,19 +48,6 @@ module.exports = [
           },
       ],
       "func-call-spacing": ["error", "never"],
-      "security/detect-buffer-noassert": 1,
-      "security/detect-child-process": 1,
-      "security/detect-disable-mustache-escape": 1,
-      "security/detect-eval-with-expression": 1,
-      "security/detect-new-buffer": 1,
-      "security/detect-no-csrf-before-method-override": 1,
-      "security/detect-non-literal-fs-filename": 1,
-      "security/detect-non-literal-regexp": 1,
-      "security/detect-non-literal-require": 0,
-      "security/detect-object-injection": 0,
-      "security/detect-possible-timing-attacks": 1,
-      "security/detect-pseudoRandomBytes": 1,
-      "security/detect-unsafe-regex": 1,
     },
     settings: {
       react: {
